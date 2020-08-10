bcc19b432036cebb9987b81e6eb4b5e776fd680a
Cengiz TORU <cengiztoru@gmail.com>
2020-08-10 18:16:57 +0300

    UI prepared

---
+++ app/src/main/java/com/cengiztoru/architecturalcomponentssample/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.cengiztoru.architecturalcomponentssample.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cengiztoru.architecturalcomponentssample.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/cengiztoru/architecturalcomponentssample/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.cengiztoru.architecturalcomponentssample.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cengiztoru.architecturalcomponentssample.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
