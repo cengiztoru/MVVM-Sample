07058d6b1228906e8e28ec729880e98257f5a9af
Cengiz TORU <cengiztoru@gmail.com>
2020-08-12 14:15:23 +0300

    Added checking internet connection for different api versions

--- app/src/main/java/com/cengiztoru/architecturalcomponentssample/data/network/NetworkConnectionsInterceptor.kt
+++ app/src/main/java/com/cengiztoru/architecturalcomponentssample/data/network/NetworkConnectionsInterceptor.kt
@@ -2,6 +2,10 @@ package com.cengiztoru.architecturalcomponentssample.data.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.cengiztoru.architecturalcomponentssample.util.NoInternetExceptions
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -11,13 +15,23 @@ class NetworkConnectionsInterceptor(context: Context) : Interceptor {
     private val applicationContext = context
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        if (!isInternetAvailable()) {
+
+        if (!checkIsNetworkAvailable()) {
             throw NoInternetExceptions("Make sure you have an active data connection")
         }
+
         return chain.proceed(chain.request())
     }
 
-    private fun isInternetAvailable(): Boolean {
+    private fun checkIsNetworkAvailable(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return isInternetAvailableNEW(applicationContext)
+        } else {
+            return isInternetAvailableOLD(applicationContext)
+        }
+    }
+
+    private fun isInternetAvailableOLD(applicationContext: Context): Boolean {
         val connectivityManager =
             applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
@@ -25,6 +39,27 @@ class NetworkConnectionsInterceptor(context: Context) : Interceptor {
         connectivityManager.activeNetworkInfo.also {
             return it != null && it.isConnected
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isInternetAvailableNEW(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+        return false
     }
 
 }
